ff3c864dcd677df98fcab9ed5cf101028222f598
Usmanqayyum109 <you@example.com>
2022-12-27 08:40:33 +0500

    challenges

---
+++ src/Componets/Challenge1/Chal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const chal = () => {
+  return (
+    <div>
+        <h1>Challenge1</h1>
+    <h3>Hafiz Muhammad Usman Qayyum</h3>
+    <p>List of 5 Best Cites </p>
+    <ol>
+        <li>Lahore</li>
+        <li>Karachi</li>
+        <li>Peshawar</li>
+        <li>Islamabad</li>
+        <li>Rawalpindi</li>
+    </ol>
+    </div>
+  )
+}
+
+export default chal
